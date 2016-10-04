1048e641047726f4f578bd7ba32fa40d3aa760ec
Jeff Schmitz <jeffeschmitz@gmail.com>
2016-10-04 06:29:41 -0500

    Coded the ContentViewController.

--- ISHPullUpSample/ISHPullUpSample/ContentVC.swift
+++ ISHPullUpSample/ISHPullUpSample/ContentVC.swift
@@ -8,8 +8,35 @@
 
 import Foundation
 import UIKit
+import MapKit
+import ISHPullUp
 
-class ContentVC: UIViewController
+class ContentVC: UIViewController, ISHPullUpContentDelegate
 {
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var layoutAnnotationLabel: UILabel!
+    // rootView rely's on edge's inset, which cannot be set in VC's view directly
+    @IBOutlet weak var rootView: UIView!
     
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        layoutAnnotationLabel.layer.cornerRadius = 2
+        
+        // the mapView should use the rootView's layout margins
+        // to correctly update the legal label and coordinate region
+        mapView.preservesSuperviewLayoutMargins = true
+    }
+    
+    // MAR: ISHPullUpContentDelegate
+    func pullUpViewController(_ pullUpViewController: ISHPullUpViewController, update edgeInsets: UIEdgeInsets, forContentViewController contentVC: UIViewController) {
+        
+        // update edgeInsets
+        rootView.layoutMargins = edgeInsets
+        
+        // call layouIfNeeded right away to participate in aninmations.
+        // this method may be called from within animageion blocks
+        rootView.layoutIfNeeded()
+    }
 }
