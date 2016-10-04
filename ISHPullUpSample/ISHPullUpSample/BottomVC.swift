c4f305b7318e5f2f54c27148842f98594313ca6c
Jeff Schmitz <jeffeschmitz@gmail.com>
2016-10-04 06:21:27 -0500

    UI changes

---
+++ ISHPullUpSample/ISHPullUpSample/BottomVC.swift
@@ -0,0 +1,21 @@
+//
+//  BottomVC.swift
+//  ISHPullUpSample
+//
+//  Created by Jeff Schmitz on 10/3/16.
+//  Copyright © 2016 Jeff Schmitz. All rights reserved.
+//
+
+import Foundation
+import ISHPullUp
+
+class BottomVC: UIViewController
+//    ,ISHPullUpSizingDelegate
+//    ,ISHPullUpStateDelegate
+{
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
--- ISHPullUpSample/ISHPullUpSample/ContentVC.swift
+++ ISHPullUpSample/ISHPullUpSample/ContentVC.swift
@@ -1,17 +1,15 @@
 //
-//  ViewController.swift
+//  ContentVC.swift
 //  ISHPullUpSample
 //
 //  Created by Jeff Schmitz on 10/3/16.
 //  Copyright © 2016 Jeff Schmitz. All rights reserved.
 //
 
+import Foundation
 import UIKit
-import ISHPullUp
 
-class ViewController: ISHPullUpViewController {
-
+class ContentVC: UIViewController
+{
     
-
 }
-
